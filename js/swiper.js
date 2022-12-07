859b2303d5b24cca9f9f6b59395db58ddd0a2e1a
Khurshid Uzokov <xurshiduzoqov1@gmail.com>
2022-12-07 18:54:09 +0500

    male a responsive drobline section

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,19 @@
-// Spraying section
+// Spraying page
 
 var swiper = new Swiper(".plusSwiper", {
     spaceBetween: 30,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+});
+
+
+
+
+var swiper = new Swiper(".drobSwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
     },
 });
